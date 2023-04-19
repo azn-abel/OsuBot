8e5daee8b9ff5f7fe295e2bf1ad7835572f49d8d
Abel Lu <aluprof4@ncsu.edu>
2023-04-19 03:02:27 -0400

    removed unnecessary code

--- bot.py
+++ bot.py
@@ -2,10 +2,7 @@ import discord
 from discord.ext import commands
 from pytz import UTC
 
-from scraping import *
-from emoji import *
 from api import refresh_token
-from datetime import date, datetime
 from customembeds import *
 
 import os
@@ -38,99 +35,6 @@ async def info(ctx, username: str, mode: str = None):
         await ctx.reply(embed=reply_embed)
     except Exception as e:
         await ctx.reply(e)
-
-
-
-    # try:
-    #     user_data, play_data = get_data(username, mode)
-    # except:
-    #     await ctx.reply('Invalid username.')
-    #     return
-    #
-    # global_rank = user_data['statistics']['global_rank']
-    #
-    # try:
-    #     most_recent_month = user_data['monthly_playcounts'][-1]
-    #     most_played = play_data['beatmapPlaycounts'][0]
-    #     top_play = play_data['scoresBest'][0]
-    # except:
-    #     most_recent_month = {
-    #         'start_date': '2021-05-01',
-    #         'count': 0
-    #     }
-    #     most_played = None
-    #     top_play = None
-    # plays_this_month = most_recent_month['count']
-    # month_start = f"{str(date.today())[:-2]}01"
-    #
-    # # START EMBED
-    # reply_embed = discord.Embed(
-    #     title=f":flag_{user_data['country_code'].lower()}: {user_data['username']}'s osu!{arg if arg else ''} Profile {mode_emoji[mode]}",
-    #     colour=discord.Colour.blue()
-    # )
-    #
-    # reply_embed.add_field(
-    #     name="Global Rank",
-    #     value=f"#{global_rank:,}" if global_rank else "Unranked")
-    # reply_embed.add_field(
-    #     name="pp",
-    #     value=f"{user_data['statistics']['pp']:,}"
-    # )
-    # reply_embed.add_field(
-    #     name='Total Play',
-    #     value=convert_time(user_data['statistics']['play_time'])[:-3] if user_data['statistics'][
-    #         'play_time'] else "None"
-    # )
-    # reply_embed.add_field(
-    #     name='Monthly Plays',
-    #     value=f"{plays_this_month}" if most_recent_month['start_date'] == month_start else "0"
-    # )
-    # reply_embed.add_field(
-    #     name='Medals',
-    #     value=str(len(user_data['user_achievements']))
-    # )
-    #
-    # try:
-    #     beatmapset = top_play['beatmapset']
-    #     mods_string = f' +{", ".join(top_play["mods"])}'
-    #
-    #     reply_embed.add_field(
-    #         name=f'Top Play [ {rank_emoji[top_play["rank"]]}{" FC" if top_play["perfect"] else ""}{mods_string if top_play["mods"] else ""} for {top_play["pp"]} pp ]',
-    #         value=f"- **Name:** {beatmapset['title']} [{top_play['beatmap']['version']}]\n"
-    #               f"- **Artist:** {beatmapset['artist']}\n"
-    #               f"- **Mapper:** {beatmapset['creator']}\n",
-    #
-    #         inline=False
-    #     )
-    # except:
-    #     reply_embed.add_field(
-    #         name='Top Play',
-    #         value="No plays",
-    #
-    #         inline=False
-    #     )
-    # try:
-    #     beatmapset = most_played['beatmapset']
-    #     reply_embed.add_field(
-    #         name='Most Played Beatmap',
-    #         value=f"- **Name:** {beatmapset['title']} [{most_played['beatmap']['version']}]\n"
-    #               f"- **Artist:** {beatmapset['artist']}\n"
-    #               f"- **Mapper:** {beatmapset['creator']}\n"
-    #               f"- **Plays:** {most_played['count']}" if most_played else "bruh",
-    #
-    #         inline=False
-    #     )
-    # except:
-    #     reply_embed.add_field(
-    #         name='Most Played Beatmap',
-    #         value="No plays",
-    #
-    #         inline=False
-    #     )
-    #
-    # # END EMBED
-
-
 
 
 @client.command(aliases=['r', 'R'])
@@ -179,14 +83,14 @@ async def top(ctx, username: str, *args: str):
             await ctx.reply(e)
 
 
-@client.command()
-async def all(ctx, username, mode="osu"):
-    user_data, extra_data = get_data(username, mode)
-
-    output_string = ""
-    for x in extra_data['scoresBest']:
-        output_string += f"{x['rank']}\n"
-    await ctx.reply(output_string)
+# @client.command()
+# async def all(ctx, username, mode="osu"):
+#     user_data, extra_data = get_data(username, mode)
+#
+#     output_string = ""
+#     for x in extra_data['scoresBest']:
+#         output_string += f"{x['rank']}\n"
+#     await ctx.reply(output_string)
 
 
 @client.command()

5f31576aa39f11ce91526feff5db4989abf5b182
Abel Lu <aluprof4@ncsu.edu>
2023-04-24 18:54:30 -0400

    initial database manager commit

---
+++ database.py
@@ -0,0 +1,32 @@
+import asyncpg
+
+
+class DatabaseManager:
+    _instance = None
+
+    # Singleton design pattern
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self, dsn):
+        self.dsn = dsn
+        self.pool = None
+
+    async def initialize(self):
+        self.pool = await asyncpg.create_pool(dsn=self.dsn)
+
+    async def execute(self, query, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.execute(query, *args)
+
+    async def fetch(self, query, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.fetch(query, *args)
+
+    async def transaction(self, queries):
+        async with self.pool.acquire() as conn:
+            async with conn.transaction():
+                for query, *args in queries:
+                    await conn.execute(query, *args)
